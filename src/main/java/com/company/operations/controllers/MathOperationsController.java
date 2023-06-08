9d2578f26f50812cf30fe10bcb2731d5cb9382cd
mlubrina <mlubrina@gmail.com>
2023-06-08 19:40:52 -0300

    initial commit

--- src/main/java/com/company/operations/controllers/MathOperationsController.java
+++ src/main/java/com/company/operations/controllers/MathOperationsController.java
@@ -4,7 +4,6 @@ import com.company.operations.entities.MathRequestDto;
 import com.company.operations.enums.OperationType;
 import com.company.operations.services.factory.OperationsFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +21,7 @@ public class MathOperationsController {
     }
 
     @PostMapping("/execute")
-    public ResponseEntity<String> executeMathOperation(@Valid @RequestBody MathRequestDto mathRequest, BindingResult bindingResult) {
+    public ResponseEntity<String> executeMathOperation(@Valid @RequestBody MathRequestDto mathRequest) {
         return ResponseEntity.ok(
                     operations.getOperationByType(OperationType.valueOf(mathRequest.getOperation().toUpperCase()))
                             .execute(mathRequest.getNumbers()));
